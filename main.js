ddd2800c878e568fefb1660f10ca6a578ea3b74d
Student_M23 <Student_M23@PS-DAL-M23.local>
2020-03-10 14:02:30 -0500

    Add existing file

---
+++ main.js
@@ -0,0 +1,48 @@
+var ussSchwarz = {
+    top: 900,
+    left: 850
+};
+
+document.onkeydown = function(e) {
+    console.log(e.keyCode);
+    if (e.keyCode === 37) {
+        console.log("LEFT");
+        ussSchwarz.left = ussSchwarz.left - 30;
+        moveUssSchwarz()
+    } 
+    else if (e.keyCode === 39) {
+        console.log("RIGHT");
+        ussSchwarz.left = ussSchwarz.left + 30;
+        moveUssSchwarz()
+    }
+    else if (e.keyCode === 32) {
+        console.log("FIRE")
+    }
+}
+
+function moveUssSchwarz() {
+document.getElementById('ussSchwarz').style.left = ussSchwarz.left + "px";
+}
+moveUssSchwarz();
+
+
+
+
+
+
+
+
+
+
+// var alien1 = {
+//     top: 120,
+//     left: 850
+// };
+
+// document.onkeydown = function(a) {
+//     console.log(a.key)
+// }
+
+
+
+
